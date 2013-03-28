2f3873857fae7d11b5eea1b1dc88242deb268c97
Andreas Hennings <andreas@dqxtech.net>
2013-03-28 14:48:21 +0100

    improve autoload, make it agnostic to root dir

--- autoload.php
+++ autoload.php
@@ -1,22 +1,29 @@
 <?php
 // Include this only if you have no other autoload solution available.
+namespace CiviCRM\API3;
 
-spl_autoload_register(function($class) {
-  $ns = 'CiviCRM\\API3\\';
-  $len = strlen($ns);
-  if ($ns === substr($class, 0, $len)) {
-    $suffix = substr($class, $len);
-    if (FALSE !== $nspos = strrpos($suffix, '\\')) {
-      $namespace = substr($suffix, 0, $nspos);
-      $classname = substr($suffix, $nspos + 1);
-      $path = 'lib/CiviCRM/API3/' . str_replace('\\', '/', $namespace) . '/' . str_replace('_', '/', $classname) . '.php';
+_init_autoload();
+
+function _init_autoload() {
+  $dir = dirname(__FILE__) . '/lib/CiviCRM/API3/';
+
+  spl_autoload_register(function($class) use ($dir) {
+    $ns = 'CiviCRM\\API3\\';
+    $len = strlen($ns);
+    if ($ns === substr($class, 0, $len)) {
+      $suffix = substr($class, $len);
+      if (FALSE !== $nspos = strrpos($suffix, '\\')) {
+        $namespace = substr($suffix, 0, $nspos);
+        $classname = substr($suffix, $nspos + 1);
+        $path = $dir . str_replace('\\', '/', $namespace) . '/' . str_replace('_', '/', $classname) . '.php';
+      }
+      else {
+        $classname = $suffix;
+        $path = $dir . str_replace('_', '/', $classname) . '.php';
+      }
+      if (is_file($path)) {
+        include $path;
+      }
     }
-    else {
-      $classname = $suffix;
-      $path = 'lib/CiviCRM/API3/' . str_replace('_', '/', $classname) . '.php';
-    }
-    if (is_file($path)) {
-      include $path;
-    }
-  }
-});
+  });
+}
